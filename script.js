6f1eb773d70ba54fec84bff7bbf4e1d64f1ddd6d
priya <shanmu23cs017@gmail.com>
2025-05-16 15:37:54 +0530

    script added

---
+++ script.js
@@ -0,0 +1,27 @@
+let a=10;
+let b=20;
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a==b)
+console.log(a!=b)
+console.log(a<=b)
+console.log(a>=b)
+console.log(a>b)
+console.log(a<b)
+
+function sub(){
+    var a=20;
+    var b=10;
+    console.log(a-b)
+}
+sub()
+ 
+function mul(a,b){
+    console.log(a*b)
+}
+mul(10,4)
+
