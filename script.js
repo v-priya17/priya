eb238c17a3e6b6c66c2bb0bbb2b22de37dbca3bd
priya <shanmu23cs017@gmail.com>
2025-05-17 14:22:20 +0530

    fourth commit

--- script.js
+++ script.js
@@ -20,8 +20,53 @@ function sub(){
 }
 sub()
  
-function mul(a,b){
+var mul=(a,b)=>{
     console.log(a*b)
 }
 mul(10,4)
 
+
+
+ a=10;
+ b=20;
+ console.log(`${a}+${b}=${a+b}`)
+
+
+ c='welcome';
+ console.log(c)
+
+
+ n=23
+ if(n%2==0){
+    console.log('even')
+ }
+ else{
+    console.log('odd')
+ }
+
+ for(i=0;i<5;i++){
+    if(i%2!=0){
+        console.log(i)
+    }
+ }
+
+ for(i=0;i<10;i++){
+    if(i%2==0){
+        console.log(`${i} is even`)
+    }
+    else{
+        console.log(`${i}is odd`)
+    }
+ }
+
+
+n=1;
+while(n<=10){
+    if(n%2==0){
+        console.log(`${n} is even`)
+    }
+    else{
+        console.log(`${n}is odd`)
+    }
+    n++;
+}
