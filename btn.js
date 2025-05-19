071b980c5fac6821c36e44572f4e104d26df9d82
priya <shanmu23cs017@gmail.com>
2025-05-19 13:03:30 +0530

    sixth commit

---
+++ btn.js
@@ -0,0 +1,7 @@
+const btn= document.getElementById("btnl");
+
+btn.onclick = function () {
+    
+    alert("hello")
+    window.open("https://www.instagram.com/")
+}
--- script.js
+++ script.js
@@ -134,5 +134,100 @@ count=0;
     console.log(a)
  }
  eadd()
- 
- 
+
+ let movie=["rosario","jo","malignent","holy night","tall grass"];
+ console.log(movie)
+ console.log(movie[4])    
+ for(let mo in movie){           
+    console.log(mo)
+ }
+ for(was of movie){
+    console.log(was)
+ }
+ var size=movie.length;
+ console.log(size)
+
+
+ ar=[1,14,11,25];
+ for(var index in ar){
+    console.log(index)
+ }
+ for(inx of ar){
+    console.log(inx)
+ }
+
+var arr=["randy","john cena","roman","sethrollins"];
+ for(var key of arr){
+    console.log(key)
+ }
+
+ var ice={brand:"arun",flavour:"vannila"}
+ for(value in ice){
+ console.log(ice[value])
+ }
+ for(key in ice){
+    console.log(key)
+ }
+for(val in ice){
+    console.log(ice[val])
+}
+
+var r=4+"priya";
+console.log(r)
+
+// spread operator
+a=["thala","gbu","aadhi","anbuarivu"];
+b=["arjundas",...a];
+console.log(b)
+console.log(a)
+console.log(a[2])
+
+// destructuring operator
+var m=[90,98,89,78];
+var [cs,ca,py,fs]=m;
+console.log(cs)
+console.log(ca)
+console.log(py)
+console.log(fs)
+const person = { name: 'John', age: 30, city: 'New York' };
+const { name, age, city } = person;
+console.log(person); 
+console.log(name,age,city)
+
+const nest= { 
+    data: [
+        { id: 1, name: 'Item 1' }, 
+        { id: 2, name: 'Item 2' }
+    ] 
+};
+const { data: [a1,a2] } = nest;
+console.log(a1.id);
+console.log(a2); // Output: Item 1
+
+const nested = { 
+    data: [
+        { id: 1, name: 'Item 1' }, 
+        { id: 2, name: 'Item 2' }
+    ] 
+};
+const { data: [{ nmae: firstItemName }] } = nested;
+console.log(firstItemName); // Output: Item 1
+
+
+
+// call back function
+function food(){
+    console.log("my fav food is curdrice");
+}
+function love(callback){
+    console.log("food lover");
+    callback();
+}
+love(food)
+
+const btn= document.getElementById();
+
+btn.onclick=function(){
+    alert("vaanga palagalam")
+    window.open("https://www.instagram.com/")
+}
